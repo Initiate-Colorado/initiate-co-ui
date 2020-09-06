3036c01da7f2e8b617a7ce46009e85b2e218d00f
Danny Ramos <danramos21@gmail.com>
2020-09-06 17:46:59 -0600

    sample test to test jest functioning properly

---
+++ api/src/modules/subscriber/sample.test.js
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import express from 'express'
+import schema from '../../setup/schema'
+import subscriber from './model'
+
+describe('subscriber models', () => {
+  
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+
+  })
+})
