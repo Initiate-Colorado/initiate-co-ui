cadb7883d81fd5848dcfde197b18b2f24dfa1aba
Danny Ramos <danramos21@gmail.com>
2020-09-06 10:59:17 -0600

    deleted crate images

--- api/src/modules/subscriber/tests/sample.test.js
+++ api/src/modules/subscriber/tests/sample.test.js
@@ -1,5 +1,5 @@
-// import request from 'supertest'
-// import express from 'express'
+import request from 'supertest'
+import express from 'express'
 
 describe('Sample Test', () => {
   it('should test that true === true', () => {
