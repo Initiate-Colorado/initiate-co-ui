72422b473d3b41e507dce7fe73f38d21ccefde0b
Danny Ramos <danramos21@gmail.com>
2020-09-06 17:46:29 -0600

    mutation test outline

---
+++ api/src/modules/user/mutations.test.js
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from '../../setup/schema'
+import models from '../../setup/models'
+import bcrypt from 'bcrypt';
+import db from '../../setup/database';
+
+describe('user mutations', () => {
+  let server;
+  beforeAll(() => {
+    server = express();
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false,
+      })
+    )
+  })
+  afterEach(async () => {
+    await models.User.destroy({ where: {} })
+  })
+  afterAll(() => {
+    db.close()
+  })
+  
+  it('can create new user', async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userSignup(name: "Tommy Boy", email: "blacksheep@movie.com", password: "12345"){id}}' })
+      .expect(200)
+    console.log(response.body.data)
+    expect(response.body.data.userSignup.id).toBeNumber()
+  })
+
+  it('can remove a user', async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userRemove (id: 1) {id}}' })
+      .expect(200)
+    expect(response.body.data.userRemove.id).toBeNull()
+  })
+})
