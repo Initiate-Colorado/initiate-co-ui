4dc1efdf83bec48962d8c1c1c60a1dd8e851e20a
Charles Wefso <cwefso@gmail.com>
2020-09-06 10:05:38 -0600

    Creates InitiativeContainer and Research Components

---
+++ client/src/components/Process/Process.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Research.css';
+import InitiativeContainer from '../InitiativeContainer/InitiativeContainer';
+
+function Research() {
+  return (
+    <div className="Research">
+        <InitiativeContainer />
+    </div>
+  );
+}
+
+export default Research;
