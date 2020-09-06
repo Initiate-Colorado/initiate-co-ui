783a4813a29be8b05f23de6c0a45068781b8eb3b
Danny Ramos <danramos21@gmail.com>
2020-09-06 10:56:36 -0600

    moved sample test to new test folder

---
+++ api/src/modules/subscriber/tests/sample.test.js
@@ -0,0 +1,8 @@
+// import request from 'supertest'
+// import express from 'express'
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
