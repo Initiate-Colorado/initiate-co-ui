5ef68de37771f8518bb86f6d95f3bc31d074f94e
Charles Wefso <cwefso@gmail.com>
2020-09-09 13:49:41 -0600

    prepare for deploy

---
+++ client/build/precache-manifest.ae1723fa5f07caf5fb9bdb48b236df15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70285502a5c7f054ca7e06c003976cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79b57a06e8f38bcc4c57",
+    "url": "/static/css/main.86bd7e5c.chunk.css"
+  },
+  {
+    "revision": "85075c6170c2bde1e046",
+    "url": "/static/js/2.6ae874dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ae874dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b57a06e8f38bcc4c57",
+    "url": "/static/js/main.bab39c15.chunk.js"
+  },
+  {
+    "revision": "9494a6dda5375ec7deed",
+    "url": "/static/js/runtime-main.38f258d5.js"
+  }
+]);
