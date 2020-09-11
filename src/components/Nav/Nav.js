b35e4659108bbca9a2c6cd95cb57df3f8a46d46c
Server <server@server.com>
2020-09-11 10:47:28 -0600

    Initial style changes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 
 function Nav() {
   return (
     <div className="Nav">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap" rel="stylesheet" />
+      <NavLink to={"/"}>
+        <img src= "../public/logo.png" />
+      </NavLink>    
       <header className="Nav-header">
         <p className="Nav-p">
-          Nav
+          INITIATE COLORADO
         </p>
         <p className="logged">
           Logged In
