2732e80b68782961899945fe20461bbd8b0b10b2
Server <server@server.com>
2020-09-12 13:51:05 -0600

    Add POST method for official ballots

--- src/components/Official/Official.js
+++ src/components/Official/Official.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
+import { query } from 'gql-query-builder'
 import './Official.css';
 
 
-function Official() {
-  return (
-    <div className="Official">
-      <h1>This will hold our official initiatives</h1>
-    </div>
-  );
-}
+
+class Official extends Component {
+
+  getOfficialBallots() {
+    return axios.post('https://initiate-co-backend.herokuapp.com/', query({
+      operation: 'ballots',
+      fields: ['id']
+    })).then(
+      response => {
+        console.log(response)
+        }
+      ).catch(function (error) {
+        console.log(error);
+      }
+    )
+  }
+
+  componentDidMount() {    
+    this.getOfficialBallots()
+  }
+
+  render () {
+    return (
+      <div className="Official">
+        <h1>This will hold our official initiatives</h1>
+      </div>
+    );
+}}
 
 export default Official;
