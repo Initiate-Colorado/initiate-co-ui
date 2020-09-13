eb5607758ce00995b467ca29689462c54117f4b7
Server <server@server.com>
2020-09-12 19:02:01 -0600

    Change Official and Proposed components to Redux syntax

--- src/components/Official/Official.js
+++ src/components/Official/Official.js
@@ -1,30 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios'
+import { connect } from "react-redux";
 import { query } from 'gql-query-builder'
 import './Official.css';
 
 
+const Official = () => {
 
-class Official extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      error: '',
-      data: {}
-    }
-  }
-
-  getOfficialBallots() {
+  const getOfficialBallots = () => {
     return axios.post('https://initiate-co-backend.herokuapp.com/', query({
       operation: 'ballots',
       fields: ['id']
     })).then(
       response => {
-        this.setState({
-          data: response.data.data.ballots
-        })
-        console.log(this.state)
+        console.log(response.data.data.ballots)
         }
       ).catch(function (error) {
         console.log(error);
@@ -32,29 +21,21 @@ class Official extends Component {
     )
   }
 
+  getOfficialBallots();
 
-  componentDidMount = async() => {
-    try {
-      const officialBallots = await this.getOfficialBallots()
-      this.setState({
-        data: officialBallots.data
-      })
-      // console.log(this.state)
-    } catch (error) {
-        this.setState({error: error})
-    }
-  }    
-  
-
-
-  render () {
-    return (
+  return (
       <div className="Official">
         <h1>This will hold our official initiatives</h1>
         {/* <h1>{this.state.data}</h1> */}
       </div>
     );
-}}
+};   
+
+const mapStateToProps = (state) => {
+  return {
+    ballots: state
+  };
+};
 
 
-export default Official;
+export default connect(mapStateToProps, null)(Official);
