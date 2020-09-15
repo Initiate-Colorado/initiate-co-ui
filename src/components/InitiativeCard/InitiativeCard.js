5854f9062caf76354092ba9f271b0a3b9559aeb2
Server <server@server.com>
2020-09-15 15:54:29 -0600

    Add display functionality

--- src/components/InitiativeCard/InitiativeCard.js
+++ src/components/InitiativeCard/InitiativeCard.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import './InitiativeCard.css';
 
 class InitiativeCard extends Component  {
@@ -11,10 +11,11 @@ class InitiativeCard extends Component  {
 
 
   render() {
-    const { ballot } = this.props
-    console.log('ballot', ballot)
-    console.log('state', this.state)
-    console.log('props', this.props)
+    // const { ballot } = this.props
+    // console.log('ballot', ballot)
+    // console.log('state', this.state)
+    console.log(this.state.ballot)
+    // console.log('props', this.props)
     return (
     <div className="InitiativeCard">
         {/* <h1>Citizenship Qualification of Electors</h1>
@@ -22,7 +23,7 @@ class InitiativeCard extends Component  {
         <p>Only a citizen of the United State who has attained the age of eighteen years,
         has resided in this state for such time as may be prescribed by law, and has been duly
         registered as a voter if required by law shall be qualified to vote at all elections.</p> */}
-        <h1>Representative:</h1>
+        <h1>Representative: </h1>
         <h2>{this.state.ballotNumber}</h2>
         <p>{this.props.title}</p>
     </div>
